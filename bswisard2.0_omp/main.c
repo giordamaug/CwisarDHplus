cc2df55baa7254ae4844adcb598ae667b499b5f1
Maurizio Giordano <maurizio@MacBook-Air-di-Maurizio.local>
2013-09-21 20:41:45 +0200

    Initial Commit

---
+++ bswisard2.0_omp/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bswisard2.0_omp
+//
+//  Created by Maurizio Giordano on 21/09/13.
+//  Copyright (c) 2013 Maurizio Giordano. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
